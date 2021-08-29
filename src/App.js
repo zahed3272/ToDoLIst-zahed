2ebb1c9b5f7fa36dbcf0bf73dcf744e10f134ff0
zahed3272 <82074435+zahed3272@users.noreply.github.com>
2021-08-29 17:13:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+// import React from 'react'
 
+// const App = () => {
+//     return (
+//         <div className="todo-app">
+//           <h1>todolist</h1>  
+//         </div>
+//     )
+// }
+
+// export default App
+  
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+import TodoFrom from './components/TodoForm'
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
