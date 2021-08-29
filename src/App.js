a5e53b1e596f8bbe5af9cc4e8647c17c8e2cecd8
zahed3272 <82074435+zahed3272@users.noreply.github.com>
2021-08-29 21:58:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
-import TodoFrom from './components/TodoForm'
+// import TodoFrom from './components/TodoForm'
 
 function App() {
   return (
